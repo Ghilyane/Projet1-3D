a2ca8f9c8cfd23850e8d9a1c1a8a3b337fca112c
alexhorga <99271417+alexhorga@users.noreply.github.com>
2023-02-15 14:40:59 -0500

    update

--- echelle.js
+++ echelle.js
@@ -2,7 +2,7 @@ function Echelle(){
     let objEchelle = new Object()
     objEchelle.X = 0
     objEchelle.Y = 0
-    objEchelle.intLargeur = 3
+    objEchelle.intLargeur = 5
     objEchelle.intHauteur = 3
     objEchelle.strCouleur = 'yellow'
 
--- or.js
+++ or.js
@@ -3,8 +3,8 @@ function Lingot(){
     let objLingot = new Object()
     objLingot.X = 0
     objLingot.Y = 0
-    objLingot.intLargeur = 7    //7 pour le moment
-    objLingot.intHauteur = 7 
+    objLingot.intLargeur = 5
+    objLingot.intHauteur = 3 
     objLingot.binRamasser = false
 
     return objLingot
