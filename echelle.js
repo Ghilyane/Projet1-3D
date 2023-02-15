c4cb13cede4e0bcb1f2686296b1fbacc4f8ca428
alexhorga <99271417+alexhorga@users.noreply.github.com>
2023-02-15 14:32:30 -0500

    objets Échelles

---
+++ echelle.js
@@ -0,0 +1,10 @@
+function Echelle(){
+    let objEchelle = new Object()
+    objEchelle.X = 0
+    objEchelle.Y = 0
+    objEchelle.intLargeur = 3
+    objEchelle.intHauteur = 3
+    objEchelle.strCouleur = 'yellow'
+
+    return objEchelle
+}
---
+++ echelleNiveau.js
@@ -0,0 +1,11 @@
+function EchelleNiveau(){
+    let objEchelleNiveau = new Object()
+    objEchelleNiveau.X = 150    //150 pour le moment
+    objEchelleNiveau.Y = 0
+    objEchelleNiveau.intLargeur = 15
+    objEchelleNiveau.intHauteur = 5
+    objEchelleNiveau.strCouleur = 'yellow'
+    objEchelleNiveau.binVisible = false
+
+    return objEchelleNiveau
+}
