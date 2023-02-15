54d5ddb59a6e4a111d604c303dc9fd6f4f4415a8
alexhorga <99271417+alexhorga@users.noreply.github.com>
2023-02-15 14:05:43 -0500

    Update or.js

---
+++ or.js
@@ -0,0 +1,11 @@
+function Lingot(){
+
+    let objLingot = new Object()
+    objLingot.X = 0
+    objLingot.Y = 0
+    objLingot.intLargeur = 7    //7 pour le moment
+    objLingot.intHauteur = 7 
+    objLingot.binRamasser = false
+
+    return objLingot
+}
