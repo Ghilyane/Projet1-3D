b16498bcf79c54b6ea92e9d63b3edb06909be60e
alexhorga <99271417+alexhorga@users.noreply.github.com>
2023-02-25 22:28:37 -0500

    update

--- ObjetsFixes/echelle.js
+++ ObjetsFixes/echelle.js
@@ -12,22 +12,17 @@ function initEchelle() {
 Echelle.prototype.dessinerEchelle = function (objC2D) {
     objC2D.save();
     objC2D.translate(intLargeur, intHauteur);
-
-    var objEchelle = new Object()
     for (var i = 0; i < tabChar.length; i++) {
         for (var j = 0; j < tabChar[0].length; j++) {
             if (tabChar[i][j] == "3") {
-                objEchelle.intX = i
-                objEchelle.intY = j
-                tabObjEchelles.push(objEchelle)
                 objC2D.drawImage(objImgEchelle, j * intLargeur, i * intHauteur, intLargeur, intHauteur);
-
             }
         }
     }
-
-
     objC2D.restore();
 }
+
+
+
 
 
